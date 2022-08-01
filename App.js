7c29494156e94ffd8ba2e849d6462c2cdb450b77
Yuki-Hub98 <wellyson.yago1@gmail.com>
2022-08-01 16:55:39 -0400

    Branch Tela de detalhes

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StatusBar } from 'react-native'
 import { Login } from './src/screens/Login'
+import {DetailsMovies } from './src/screens/DetailsMovies'
 
 export const App = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Login />
-      <StatusBar />
+    <DetailsMovies/>
     </View>
   )
 }
