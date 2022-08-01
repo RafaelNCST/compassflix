f1ce37bb5a687016bd2f95bda27d804bb4dac476
rafaelnc <rafaelnccolares@gmail.com>
2022-08-01 15:33:26 -0400

    First commit

---
+++ src/screens/DetailsMovie/index.js
@@ -0,0 +1,12 @@
+import React from 'react-native';
+import { View, Text } from 'react-native';
+
+import { styles } from './style'
+
+export const DetailsScreen = () => {
+    return (
+        <View style={styles.bodyScreen}>
+            <Text> DetailsScreen </Text>
+        </View>
+    )
+}
---
+++ src/screens/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react-native';
+import { View, Text } from 'react-native';
+
+import { styles } from './style'
+
+export const Home = () => {
+    return (
+        <View style={styles.bodyScreen}>
+            <Text> Home </Text>
+        </View>
+    )
+}
