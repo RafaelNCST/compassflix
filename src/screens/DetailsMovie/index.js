b3281283baf7be417b93a3d5ffc450f27864a984
Yuki-Hub98 <wellyson.yago1@gmail.com>
2022-08-01 23:09:16 -0400

    other adjustments

--- src/screens/DetailsMovie/index.js
+++ src/screens/DetailsMovie/index.js
@@ -1,8 +1,10 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
-import { styles } from './style'
+import { styles } from './style';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 export const DetailsMovie = () => {
+    
     return (
         <View style={styles.bodyScreen}>
             <Text> DetailsScreen </Text>
--- src/screens/DetailsMovie/style.js
+++ src/screens/DetailsMovie/style.js
@@ -2,6 +2,8 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     bodyScreen:{
+        alignItems: "center",
+        backgroundColor:"#454545"
         
     }
 })
