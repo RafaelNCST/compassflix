e1811370d28deba947100be121500a83780a59ec
Yuki-Hub98 <wellyson.yago1@gmail.com>
2022-08-01 23:08:26 -0400

    adding mocks: cast and movie

---
+++ src/helpers/PosterMovie.js
@@ -0,0 +1,16 @@
+import poster from '../assets/Capa_Batman.jpg';
+import perfil from '../assets/Poster_Batman.jpg';
+
+export const PosterMovie = [
+
+    {
+        id: 1,
+        Image: poster,
+        Image:perfil,
+        titulo: "Batman",
+        diretor: "Matt Reeves",
+        ano: "2022",
+        duracao:"176",
+        descricao:" DESCUBRA A VERDADE. \nEm seu segundo ano de combate ao crime, Batman descobre a corrupção em Gotham City que se conecta à sua própria família enquanto enfrenta um serial killer conhecido como Charada."
+    }
+]
