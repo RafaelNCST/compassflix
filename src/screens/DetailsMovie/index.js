83b6bfe13bf3818ddc631e316b33a99282e9c9f7
Yuki-Hub98 <wellyson.yago1@gmail.com>
2022-08-01 21:47:00 -0400

    DetailsMovie

--- src/screens/DetailsMovie/index.js
+++ src/screens/DetailsMovie/index.js
@@ -1,10 +1,8 @@
-import React from 'react-native';
 import { View, Text } from 'react-native';
 import React from 'react';
-
 import { styles } from './style'
 
-export const DetailsScreen = () => {
+export const DetailsMovie = () => {
     return (
         <View style={styles.bodyScreen}>
             <Text> DetailsScreen </Text>
