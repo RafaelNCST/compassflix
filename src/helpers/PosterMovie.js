67809aa285e1a5bac99f287bfbac7c77942721bc
Yuki-Hub98 <wellyson.yago1@gmail.com>
2022-08-03 00:50:53 -0400

    I changed the language of some objects

--- src/helpers/PosterMovie.js
+++ src/helpers/PosterMovie.js
@@ -1,5 +1,5 @@
 import poster from '../assets/DetailMovies/Poster_Batman.png';
-import perfil from '../assets/DetailMovies/Perfil_Batman.jpg';
+import perfil from '../assets/DetailMovies/Perfil_Batman.png';
 
 export const PosterMovie = {
 
@@ -7,10 +7,11 @@ export const PosterMovie = {
         id: 1,
         imagePoster: poster,
         imagePerfil:perfil,
-        title: "Batman",
-        diretor: "Matt Reeves",
-        ano: "2022",
-        duracao:"176",
-        descricao:" DESCUBRA A VERDADE. \nEm seu segundo ano de combate ao crime, Batman descobre a corrupção em Gotham City que se conecta à sua própria família enquanto enfrenta um serial killer conhecido como Charada."
+        title: "The Batman",
+        director: "Matt Reeves",
+        year: "2022",
+        duration:"176",
+        note:"8.5/10",
+        description:"DESCUBRA A VERDADE. \n\nEm seu segundo ano de combate ao crime, Batman descobre a corrupção em Gotham City que se conecta à sua própria família enquanto enfrenta um serial killer conhecido como Charada."
 
 }
