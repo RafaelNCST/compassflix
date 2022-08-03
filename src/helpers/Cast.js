073e2129b88d4bdec32295683794b605ded5aa4b
Yuki-Hub98 <wellyson.yago1@gmail.com>
2022-08-03 00:42:00 -0400

    Mock changes

---
+++ src/helpers/Cast.js
@@ -0,0 +1,38 @@
+import robert from "../assets/robert_pattinson.jpg";
+import colin from "../assets/Colin.jpg";
+import Paul from "../assets/Paul.jpg";
+import zoe from "../assets/zoe.jpg";
+import jefrey from "../assets/jefrey.jpg";
+
+export const Cast = [
+    {
+        id: 1,
+        Image: robert,
+        nome: "Robert Pattinson",
+        personagem: "Bruce Wayne / The Batman",
+    },
+    {
+        id: 2,
+        Image: zoe,
+        nome: "Zoë Kravitz",
+        personagem: "Selina Kyle / Catwoman",
+    },
+    {
+        id: 3,
+        Image: Paul,
+        nome: "Paul Dano",
+        personagem: "Edward Nashton / The Riddler",
+    },
+    {
+        id: 4,
+        Image: colin,
+        nome: "Colin Farrell",
+        personagem: "Oswald 'Oz' Cobblepot / The Penguin",
+    },
+    {
+        id: 5,
+        Image: jefrey,
+        nome: "Jeffrey Wright",
+        personagem: "Lt. James Gordon",
+    }
+]
