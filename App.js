bd92ddd95b1bd8ff174b404326513cbb49f5479e
Yuki-Hub98 <wellyson.yago1@gmail.com>
2022-08-04 00:00:32 -0400

    I removed the old photos and added the ones from figma

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text} from 'react-native';
 import {DetailsMovie} from './src/screens/DetailsMovie';
+import {Cast} from './src/helpers/Cast';
 
 export const App = () => {
   return (
