53aee7c18a9c5fb1e332de248313e414662443bb
LuizaCalife <109395661+LuizaCalife@users.noreply.github.com>
2022-08-04 19:58:34 -0300

    finished the design of login screen

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { View } from 'react-native'
 import { Login } from './src/screens/Login'
 
 export const App = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Login />
-    </View>
+    <Login/>
   )
 }
 
