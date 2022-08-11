1e59788659258a77f03560b05beb6c9dbbc4a190
Yuki-Hub98 <wellyson.yago1@gmail.com>
2022-08-11 01:23:40 -0400

    adding modal styling

--- src/screens/DetailsMovie/components/DetailMovie/style.js
+++ src/screens/DetailsMovie/components/DetailMovie/style.js
@@ -112,4 +112,58 @@ export const styles = StyleSheet.create({
         fontSize: 14,
         marginTop: 14
     },
+
+    //MODAL STYLES
+    modalScreens: {
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.8)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    modalArea:{ 
+        width: '80%', 
+        height: 400, 
+        backgroundColor: '#1C2227', 
+        borderRadius: 10 
+    },
+    iconeArea:{ 
+        width: '100%', 
+        height: 30, 
+        alignItems: 'flex-end', 
+        justifyContent: 'center' 
+    },
+    iconeButton:{ 
+        marginRight: 10 
+    },
+    icone:{
+        width:30, 
+        height:30, 
+        backgroundColor:'white', 
+        alignItems:'center', 
+        justifyContent:'center', 
+        borderRadius:30
+    },
+    infoModol:{ 
+        flex: 1, 
+        alignItems: 'center' 
+    },
+    titleModal:{ 
+        fontSize: 20, 
+        color: 'white' 
+    },
+    genresArea:{ 
+        flexDirection: 'row', 
+        justifyContent: 'center',
+        width: 80, 
+        margin:3, 
+        height:25, 
+        borderRadius:15
+    },
+    genresText:{ 
+        color: 'white',
+        alignItems:'center',
+        justifyContent:'center'
+        
+    }
+
 })
