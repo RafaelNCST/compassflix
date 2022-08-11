99b3262fbd7c79d501c9b98126476b68a483039d
rafaelnc <rafaelnccolares@gmail.com>
2022-08-11 12:16:35 -0400

    add global component for loading screens content

---
+++ src/components/LoadingScreensApis/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View } from "react-native";
+
+import { styles } from './style'
+
+import Lottie from 'lottie-react-native'
+
+export const LoadingScreensApis = () => {
+    <View style={styles.loadingApis}>
+        <View style={styles.containerAnimationLoading}>
+            <Lottie
+                source={require('../../assets/lottie/movie-loading.json')}
+                loop={true}
+                autoPlay
+                resizeMode='contain'
+            />
+        </View>
+    </View>
+}
