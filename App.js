05409ec7b10641bb7236be2956e85b4513cbf3eb
rafaelnc <rafaelnccolares@gmail.com>
2022-08-12 01:53:37 -0400

    add status bar

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { LoginContextProvider } from './src/contexts/loginContext';
 import { RootStacks } from './src/routes/rootRoutes';
+import { StatusBar } from 'react-native';
 
 export const App = () => {
   return (
     <LoginContextProvider>
       <RootStacks />
+      <StatusBar backgroundColor={'#FFFFFF'} barStyle='dark-content' />
     </LoginContextProvider>
   )
 }
