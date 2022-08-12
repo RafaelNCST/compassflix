097e4618baeada521e34e5a38eb41922893d5a5d
Yuki-Hub98 <wellyson.yago1@gmail.com>
2022-08-11 20:39:57 -0400

    correções de erros

--- src/helpers/genresObj.js
+++ src/helpers/genresObj.js
@@ -1,6 +1,7 @@
+const ficcao = "Ficção cientifica"
 export const genresColors = {
     Ação: "#D11A08", Animação: "#0B9AEB", Aventura: "#116F3D", Comédia: "#E4EB0B", Documentário: "#0B38C4",
-    Fantasia: "#B40ED8", Faroeste: "#D8920E", Guerra: "#AE2323", Erótico: "#131313",
+    Fantasia: "#B40ED8", ficcao:"#92BDC5", Faroeste: "#D8920E", Guerra: "#AE2323", Erótico: "#131313",
     Romance: "#E355C5", Suspense: "#80A11A", Terror: "#6D6D6D", Drama: "#9E2F0E", História: "#01160B",
     Música: "#CA579C", Thriller: "#5A97B8", Mistério: "#5DAD40", Crime: "#163FB3", Família: "#E69507"
 }
