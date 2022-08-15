2eb08637faff0ae8a7dad0d33183d2a2264b37e2
rafaelnc <rafaelnccolares@gmail.com>
2022-08-15 16:18:41 -0400

    style: add semicolon and line breaks

--- src/helpers/genresObj.js
+++ src/helpers/genresObj.js
@@ -1,6 +1,22 @@
 export const genresColors = {
-    Ação: "#D11A08", Animação: "#0B9AEB", Aventura: "#116F3D", Comédia: "#510443", Documentário: "#0B38C4",
-    Fantasia: "#B40ED8", Faroeste: "#D8920E", Guerra: "#AE2323", Erótico: "#131313",
-    Romance: "#E355C5", Suspense: "#80A11A", Terror: "#6D6D6D", Drama: "#9E2F0E", História: "#01160B",
-    Música: "#CA579C", Thriller: "#5A97B8", Mistério: "#5DAD40", Crime: "#163FB3", Família: "#E69507", "Ficção científica": "#92BDC5"
-}
+    Ação: '#D11A08',
+    Animação: '#0B9AEB',
+    Aventura: '#116F3D',
+    Comédia: '#510443',
+    Documentário: '#0B38C4',
+    Fantasia: '#B40ED8',
+    Faroeste: '#D8920E',
+    Guerra: '#AE2323',
+    Erótico: '#131313',
+    Romance: '#E355C5',
+    Suspense: '#80A11A',
+    Terror: '#6D6D6D',
+    Drama: '#9E2F0E',
+    História: '#01160B',
+    Música: '#CA579C',
+    Thriller: '#5A97B8',
+    Mistério: '#5DAD40',
+    Crime: '#163FB3',
+    Família: '#E69507',
+    'Ficção científica': '#92BDC5',
+};
