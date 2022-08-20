bcecef042065a821475fdfce988a4ebaf4ee86e5
rafaelnc <rafaelnccolares@gmail.com>
2022-08-19 23:56:10 -0400

    style: add new styled components global

--- src/components/StyledComponents/GlobalStyleds.js
+++ src/components/StyledComponents/GlobalStyleds.js
@@ -5,3 +5,11 @@ export const BodyScreen = styled.View`
     background-color: #000000;
     align-items: center;
 `;
+
+export const TextInfos = styled.Text`
+    font-family: ${props => props.fontFamily};
+    color: ${props => props.color};
+    line-height: 15px;
+    text-align: center;
+    font-size: 11px;
+`;
