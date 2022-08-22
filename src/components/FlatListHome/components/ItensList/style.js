ee2f284ddab8275c81476a9ba3b94286a6e75d19
Taylor Collyer <CollyerXTS>
2022-08-22 16:59:55 -0300

    Fix:Styled-Componnent-different-library-refactory

--- src/components/FlatListHome/components/ItensList/style.js
+++ src/components/FlatListHome/components/ItensList/style.js
@@ -1,34 +1,34 @@
 import styled from 'styled-components/native';
 
 export const ImagePopularMovies = styled.TouchableOpacity`
-    margin-horizontal: 8;
-    padding-vertical: 15;
-    align-items: 'center';
+    margin-horizontal: 8px;
+    padding-vertical: 15px;
+    align-items: center;
 `;
 
 export const ImageContainer = styled.View`
-    width: 76;
-    height: 95;
-    border-radius: 10;
+    width: 76px;
+    height: 95px;
+    border-radius: 10px;
 `;
 
 export const ImagePoster = styled.Image`
-    height: 95;
-    width: 76;
-    resize-mode: 'contain';
-    border-radius: 10;
+    height: 95px;
+    width: 76px;
+    object-fit: contain;
+    border-radius: 10px;
 `;
 
 export const SubContainerPopularMovies = styled.View`
-    flex-direction: 'row';
-    margin-top: 1;
-    justify-content: 'flex-start';
-    width: '100%';
+    flex-direction: row;
+    margin-top: 1px;
+    justify-content: flex-start;
+    width: 100%;
 `;
 
 export const TextPopularMovies = styled.Text`
     color: #ffffff;
-    margin-left: 4.5;
-    font-size: 9;
+    margin-left: 4.5px;
+    font-size: 9px;
     font-family: 'OpenSans-SemiBold';
 `;
