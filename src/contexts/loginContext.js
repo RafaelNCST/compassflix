22b2e84a907c6025525f10ccd23422edad8243e7
LuizaCalife <109395661+LuizaCalife@users.noreply.github.com>
2022-08-23 16:04:54 -0300

    style: removed useless line

--- src/contexts/loginContext.js
+++ src/contexts/loginContext.js
@@ -1,6 +1,5 @@
 import React, { createContext, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { get } from 'react-native/Libraries/Utilities/PixelRatio';
 
 export const LoginContext = createContext();
 
