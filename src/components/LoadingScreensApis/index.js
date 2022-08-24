25d957b4df5d50f6a85cec56641204d7ad9b6f4c
Taylor Collyer <CollyerXTS>
2022-08-24 15:45:03 -0300

    refactor:Styled-LoadingScrensApis

--- src/components/LoadingScreensApis/index.js
+++ src/components/LoadingScreensApis/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { View } from 'react-native';
 
 import { styles } from './style';
-
+import * as Styled from './style';
 import Lottie from 'lottie-react-native';
 
 export const LoadingScreensApis = () => {
     return (
-        <View style={styles.loadingApis}>
-            <View style={styles.containerAnimationLoading}>
+        <Styled.loadingApis>
+            <Styled.containerAnimationLoading>
                 <Lottie
                     source={require('../../assets/lottie/movie-loading.json')}
                     loop={true}
                     autoPlay
                     resizeMode='contain'
                 />
-            </View>
-        </View>
+            </Styled.containerAnimationLoading>
+        </Styled.loadingApis>
     );
 };
