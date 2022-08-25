05eead0cc415da7f34e67b77d264d5693808895e
Taylor Collyer <CollyerXTS>
2022-08-25 11:04:37 -0300

    refactor:Styled-DatailsMovie

--- src/screens/DetailsMovie/index.js
+++ src/screens/DetailsMovie/index.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect, useContext } from "react";
-import { useNavigation, useRoute } from "@react-navigation/native";
-import { View } from "react-native";
-import { styles } from "./style";
-import { CreditsComponent } from "./components/Credits";
-import { DetailsMovieComponent } from ".//components/DetailMovie";
-import { instance } from "../../services/api";
-import { LoginContext } from "../../contexts/loginContext";
-import { LoadingScreensApis } from "../../components/LoadingScreensApis";
+import React, { useState, useEffect, useContext } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { View } from 'react-native';
+import { styles } from './style';
+import { CreditsComponent } from './components/Credits';
+import { DetailsMovieComponent } from './/components/DetailMovie';
+import { instance } from '../../services/api';
+import { LoginContext } from '../../contexts/loginContext';
+import { LoadingScreensApis } from '../../components/LoadingScreensApis';
+import * as Styled from './style';
 
 export const DetailsMovie = () => {
     const { sessionId } = useContext(LoginContext);
@@ -24,79 +25,77 @@ export const DetailsMovie = () => {
     const { idFilmes } = useRoute().params;
 
     const getStates = async () => {
-        await instance.get(`movie/${idFilmes}/account_states?&session_id=${sessionId}`)
-            .then((resp) => {
+        await instance
+            .get(`movie/${idFilmes}/account_states?&session_id=${sessionId}`)
+            .then(resp => {
                 setMovieStates(resp?.data);
-                console.log(resp?.data?.favorite)
-
+                console.log(resp?.data?.favorite);
             })
-            .catch((error) => {
-                if(error.response){
+            .catch(error => {
+                if (error.response) {
                     console.log(error.response.data);
-                }else if(error.request){
+                } else if (error.request) {
                     console.log(error.request.data);
-                }else{
+                } else {
                     console.log('Error', error.message);
                 }
-            })
-    }
+            });
+    };
 
     const getDetail = async () => {
         await instance
             .get(`movie/${idFilmes}?&language=pt-BR`)
-            .then((resp) => {
+            .then(resp => {
                 setDetail(resp.data);
-            
             })
-            .catch((error) => console.log(error));
+            .catch(error => console.log(error));
     };
 
     const getCast = async () => {
         await instance
             .get(`movie/${idFilmes}/credits?&language=pt-BR`)
-            .then((resp) => {
+            .then(resp => {
                 setCast(resp.data.cast);
                 setCrew(resp.data.crew);
             })
-            .catch((error) => console.log(error));
+            .catch(error => console.log(error));
     };
 
-        useEffect(() => {
-            getDetail();
-            getCast();
-            setTimeout(() => {
-                setLoading(true);
-            }, 2000);
-        }, []);
+    useEffect(() => {
+        getDetail();
+        getCast();
+        setTimeout(() => {
+            setLoading(true);
+        }, 2000);
+    }, []);
 
-        useEffect(() => {
-            getStates();
-        },[noteAvaliation, markFavorite])
+    useEffect(() => {
+        getStates();
+    }, [noteAvaliation, markFavorite]);
 
-        return (
-            <View style={styles.bodyScreen}>
-                {loading ? (
-                    <>
-                        <DetailsMovieComponent
-                            Navigation={Navigation}
-                            detail={detail}
-                            visible={visible}
-                            setVisible={setVisible}
-                            directorArray={crew}
-                            note={note}
-                            setNote={setNote}
-                            noteAvaliation={noteAvaliation}
-                            setNoteAvaliation={setNoteAvaliation}
-                            movieStates = {movieStates}
-                            markFavorite ={markFavorite}
-                            setMarkFavorite={setMarkFavorite}
-                            
-                        />
-                        <CreditsComponent cast={cast} />
-                    </>
-                ) : (
-                    <LoadingScreensApis />
-                )}
-            </View>
-        );
-    };
+    return (
+        <Styled.BodyScreen>
+            {loading ? (
+                <>
+                    <DetailsMovieComponent
+                        Navigation={Navigation}
+                        detail={detail}
+                        visible={visible}
+                        setVisible={setVisible}
+                        directorArray={crew}
+                        note={note}
+                        setNote={setNote}
+                        noteAvaliation={noteAvaliation}
+                        setNoteAvaliation={setNoteAvaliation}
+                        movieStates={movieStates}
+                        markFavorite={markFavorite}
+                        setMarkFavorite={setMarkFavorite}
+                    />
+                    <CreditsComponent cast={cast} />
+                </>
+            ) : (
+                <LoadingScreensApis />
+            )}
+        </Styled.BodyScreen>
+    );
+};
