fc0fce57d3ca9c9ea07273986be7cc72303d582d
Taylor Collyer <CollyerXTS>
2022-08-25 13:38:28 -0300

    Refactor:Styled-DatailsMovie-Detail

--- src/screens/DetailsMovie/components/Detail/index.js
+++ src/screens/DetailsMovie/components/Detail/index.js
@@ -1,34 +1,50 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Image} from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { styles } from './styles'
-
+import { styles } from './styles';
+import * as Styled from './styles';
 import { genresColors } from '../../../../helpers/genresObj';
 
 export const Detail = ({ imageMovie, titleMovie, genreMovie, setVisible }) => {
     return (
-    
-        <View style={styles.modalScreens}>
-            <View style={styles.modalArea}>
-                <View style={styles.iconeArea}>
-                    <TouchableOpacity style={styles.iconeButton} onPress={() => setVisible(false)}>
+        <Styled.ModalScreens>
+            <Styled.ModalArea>
+                <Styled.IconeArea>
+                    <Styled.IconeButton onPress={() => setVisible(false)}>
                         <Icon name='clear' size={20} color={'black'} />
-                    </TouchableOpacity>
-                </View>
+                    </Styled.IconeButton>
+                </Styled.IconeArea>
                 <View style={styles.infoModal}>
-                    <Image style={styles.imageModal} source={{ uri: `https://image.tmdb.org/t/p/original${imageMovie}` }} />
+                    <Image
+                        style={styles.imageModal}
+                        source={{
+                            uri: `https://image.tmdb.org/t/p/original${imageMovie}`,
+                        }}
+                    />
                     <Text style={styles.titleModal}>{titleMovie}</Text>
                     <View style={styles.genresContainer}>
                         {genreMovie.map((item, index) => (
-                            <View key={index} style={[styles.genresArea, { backgroundColor: genresColors[item?.name], borderRadius: 15 }]}>
-                                <Text style={styles.genresText}>{(item?.name).length > 8 ? (item?.name).substring(0, 8) + '.' : (item?.name)}</Text>
+                            <View
+                                key={index}
+                                style={[
+                                    styles.genresArea,
+                                    {
+                                        backgroundColor:
+                                            genresColors[item?.name],
+                                        borderRadius: 15,
+                                    },
+                                ]}
+                            >
+                                <Text style={styles.genresText}>
+                                    {(item?.name).length > 8
+                                        ? (item?.name).substring(0, 8) + '.'
+                                        : item?.name}
+                                </Text>
                             </View>
-                        )
-                        )}
+                        ))}
                     </View>
                 </View>
-            </View>
-        </View>
-
-    )
-}
+            </Styled.ModalArea>
+        </Styled.ModalScreens>
+    );
+};
--- src/screens/DetailsMovie/components/Detail/styles.js
+++ src/screens/DetailsMovie/components/Detail/styles.js
@@ -1,71 +1,72 @@
+import styled from 'styled-components/native';
 
-import { StyleSheet } from "react-native";
+export const ModalScreens = styled.View`
+    flex: 1;
+    background-color: rgba(0, 0, 0, 0.8);
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ModalArea = styled.View`
+    width: 80%;
+    height: 400px;
+    background-color: #1c2227;
+    border-radius: 10px;
+`;
+
+export const IconeArea = styled.View`
+    width: 100%;
+    height: 50px;
+    align-items: flex-end;
+    justify-content: center;
+`;
+
+export const IconeButton = styled.TouchableOpacity`
+    margin-right: 10px;
+    width: 30px;
+    height: 30px;
+    background-color: white;
+    align-items: center;
+    justify-content: center;
+    border-radius: 30px;
+`;
 
 export const styles = StyleSheet.create({
-
-modalScreens: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.8)',
-    justifyContent: 'center',
-    alignItems: 'center'
-},
-modalArea: {
-    width: '80%',
-    height: 400,
-    backgroundColor: '#1C2227',
-    borderRadius: 10
-},
-iconeArea: {
-    width: '100%',
-    height: 50,
-    alignItems: 'flex-end',
-    justifyContent: 'center'
-},
-iconeButton: {
-    marginRight: 10,
-    width: 30,
-    height: 30,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 30
-},
-infoModal: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-},
-imageModal: {
-    width: 116,
-    height: 180,
-    borderRadius: 8,
-},
-titleModal: {
-    fontSize: 20,
-    color: 'white',
-    marginVertical: 15,
-    fontFamily: 'OpenSans-Bold'
-},
-genresContainer: {
-    width: '100%',
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center'
-},
-genresArea: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 80,
-    margin: 3,
-    height: 25,
-    borderRadius: 15,
-},
-genresText: {
-    color: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontFamily: 'OpenSans-SemiBold'
-
-}
-})
+    infoModal: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    imageModal: {
+        width: 116,
+        height: 180,
+        borderRadius: 8,
+    },
+    titleModal: {
+        fontSize: 20,
+        color: 'white',
+        marginVertical: 15,
+        fontFamily: 'OpenSans-Bold',
+    },
+    genresContainer: {
+        width: '100%',
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    },
+    genresArea: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 80,
+        margin: 3,
+        height: 25,
+        borderRadius: 15,
+    },
+    genresText: {
+        color: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily: 'OpenSans-SemiBold',
+    },
+});
