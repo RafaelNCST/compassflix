6574ed080a40aafdd47acf04278f0abe3ff09cb5
Yuki-Hub98 <wellyson.yago1@gmail.com>
2022-08-26 00:16:30 -0400

    feat: add drop down menu

---
+++ src/screens/DetailsSerie/components/ListSeason/styles.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native'
+
+
+export const styles = StyleSheet.create({
+
+    formatButtom:{
+        marginTop:5,
+        flexDirection:'row', 
+        width:'90%', 
+        height:40, 
+        left:15, 
+        backgroundColor:'rgba(255, 255, 255, 0.5)',
+        overflow:'hidden',
+        borderRadius:5 
+    },
+    textButtom:{
+        color:'#FFFFFF', 
+        top:8,
+        marginLeft:15
+    },
+    iconDrop:{
+        top:8
+    },
+    lineButtom:{
+        left:15, 
+        width:'90%', 
+        height:5, 
+        backgroundColor:'#E9A6A6', 
+        bottom:5, 
+        borderBottomRightRadius:5,
+        borderBottomLeftRadius:5
+    }
+
+
+
+})
