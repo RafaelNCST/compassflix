513127dabc14de347d2291815c23b5d7bec488e5
LuizaCalife <109395661+LuizaCalife@users.noreply.github.com>
2022-09-06 22:52:13 -0300

    feat: modal created

--- src/screens/ListScreen/components/ButtonAdd/index.js
+++ src/screens/ListScreen/components/ButtonAdd/index.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import * as Styled from './style';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Modal } from 'react-native';
 
 export const ButtonAdd = () => {
+    const [visible, setVisible] = useState(false);
     return (
-        <Styled.TestScreenView>
-            <Styled.AddButtonList>
+        <Styled.ScreenView>
+            <Styled.AddButtonList onPress={() => setVisible(true)}>
                 <Icon name='add' color='#000' size={40} />
             </Styled.AddButtonList>
-        </Styled.TestScreenView>
+            <Modal animationType='fade' transparent={true} visible={visible}>
+                <Styled.ListBodyModal>
+                    <Styled.ListContainerModal>
+                        <Styled.TextListModal>Nova lista</Styled.TextListModal>
+                        <Styled.TopInputModal>
+                            Nome da lista
+                        </Styled.TopInputModal>
+                        <Styled.BottomInputModal>
+                            Descrição
+                        </Styled.BottomInputModal>
+                        <Styled.BottomView>
+                            <Styled.ButtonModal>
+                                <Styled.CancelText
+                                    onPress={() => setVisible(false)}
+                                >
+                                    CANCELAR
+                                </Styled.CancelText>
+                            </Styled.ButtonModal>
+                            <Styled.ButtonModal>
+                                <Styled.SaveText>SALVAR</Styled.SaveText>
+                            </Styled.ButtonModal>
+                        </Styled.BottomView>
+                    </Styled.ListContainerModal>
+                </Styled.ListBodyModal>
+            </Modal>
+        </Styled.ScreenView>
     );
 };
--- src/screens/ListScreen/components/ButtonAdd/style.js
+++ src/screens/ListScreen/components/ButtonAdd/style.js
@@ -1,8 +1,7 @@
 import styled from 'styled-components/native';
 
-export const TestScreenView = styled.View`
+export const ScreenView = styled.View`
     flex: 1;
-    background-color: #fff;
     align-items: center;
     justify-content: center;
 `;
@@ -16,4 +15,74 @@ export const AddButtonList = styled.TouchableOpacity`
     position: absolute;
     top: 650px;
     right: 50px;
+`;
+export const ListBodyModal = styled.View`
+    background-color: rgba(0, 0, 0, 0.8);
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`;
+export const ListContainerModal = styled.View`
+    width: 350px;
+    height: 200px;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+    border-radius: 25px;
+`;
+export const TextListModal = styled.Text`
+    font-weight: bold;
+    color: #000;
+    font-size: 14px;
+    margin-bottom: 15px;
+`;
+export const TopInputModal = styled.TextInput`
+    background-color: rgba(196, 196, 196, 0.35);
+    width: 290px;
+    height: 34px;
+    color: #8e8e8e;
+    border-radius: 7px;
+    font-size: 13px;
+    padding-horizontal: 15px;
+`;
+export const BottomInputModal = styled.TextInput`
+    margin-top: 5px;
+    background-color: rgba(196, 196, 196, 0.35);
+    width: 290px;
+    height: 55px;
+    color: #8e8e8e;
+    border-radius: 7px;
+    font-size: 13px;
+    padding-horizontal: 15px;
+    padding-bottom: 30px;
+`;
+export const ButtonModal = styled.TouchableOpacity`
+    align-items: center;
+    justify-content: center;
+    border: 1px #000000;
+    border-radius: 5px;
+    width: 90px;
+    height: 20px;
+`;
+export const BottomView = styled.View`
+    margin-top: 15px;
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-evenly;
+    padding-horizontal: 65px;
+`;
+export const SaveText = styled.Text`
+    font-weight: bold;
+    font-size: 10px;
+    color: #fff;
+    background-color: #000;
+    height: 100%;
+    width: 100%;
+    text-align: center;
+    padding-top: 2px;
+`;
+export const CancelText = styled.Text`
+    font-weight: bold;
+    font-size: 10px;
+    color: #000;
 `;
